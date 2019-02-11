e7ea330f067a85aa9346f9975a6224fa187940de
Hoorooroob <kth5604@gmail.com>
2019-02-12 05:00:33 +0900

    [Etc]TravisCI test2

--- src/config/settings.py
+++ src/config/settings.py
@@ -12,7 +12,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
-from .dbsetting import dbsetting
+#from .dbsetting import dbsetting
+from .fake_dbsetting import dbsetting
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
